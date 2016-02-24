3edc5cf24fceeb16cbb61d23573f0fb64d1060cf
Amanej <aman.mender@tegningtil3d.no>
2016-02-24 14:44:02 +0100

    Need to properly retrieve images

--- client/templates/test/test.js
+++ client/templates/test/test.js
@@ -2,5 +2,12 @@
 Template.test.helpers({
   'modelpictures': function() {
     return Modelpictures.find();
+  },
+  'models': function() {
+    return Models.find();
+  },
+  'modelimgs': function(id) {
+    var picId = Modelimages.find({modelId: id}).fetch().name;
+    console.log(picId);
   }
-})
+});
--- client/templates/upload/upload.js
+++ client/templates/upload/upload.js
@@ -4,21 +4,43 @@ Template.upload.events({
     var title = event.target.title.value;
     var description = event.target.description.value;
     var category = event.target.category.value;
-
     var images = event.target.images.files;
-    for(var i = 0, ln = images.length; i < ln;i++) {
-      console.log(images[i].name);
 
-      Modelpictures.insert(images[i], function(err, fileObj) {
-        if(err) {
-          console.log(err);
-          FlashMessages.sendError(err.reason);
-          Router.go('upload');
-        } else {
-          console.log("Upload successful");
-        }
-      });
-    }
+    Models.insert({
+      title: title,
+      description: description,
+      category: category
+    }, function(err, id) {
+
+      modelid = id;
+
+      for(var i = 0, ln = images.length; i < ln;i++) {
+
+        var picname = images[i].name;
+
+
+        Modelpictures.insert(images[i], function(err, fileObj) {
+          if(err) {
+            console.log(err);
+            FlashMessages.sendError(err);
+            Router.go('upload');
+          } else {
+            console.log("Upload successful");
+            console.log(modelid);
+            console.log(fileObj);
+
+            Modelimages.insert({
+              modelid: modelid,
+              name: picname
+            }, function(err, id) {
+              console.log(err);
+            });
+
+          }
+        });
+      }
+    });
+
     console.log(images);
 
     console.log(title);
